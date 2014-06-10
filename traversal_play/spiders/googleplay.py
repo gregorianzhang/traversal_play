82ba381b7d402409bb3eb2397a1a2fc3eccc0963
gregorian <gregorian.zhang@gmail.com>
2014-06-11 01:55:46 +0800

    change devemail

--- traversal_play/spiders/googleplay.py
+++ traversal_play/spiders/googleplay.py
@@ -9,16 +9,16 @@ class GoogleplaySpider(Spider):
     name = "googleplay"
     allowed_domains = ["play.google.com"]
     start_urls = (
-        'https://play.google.com/store/apps/category/TOOLS/collection/topselling_free?start=0&num=24',
+        'https://play.google.com/store/apps/category/GAME_STRATEGY/collection/topselling_free?start=0&num=24',
         )
     global category1
-    global url1 
-    category1 = 'TOOLS'
+    global url1
+    category1 = 'GAME_STRATEGY'
     url1 = 'https://play.google.com'
 
     def parse(self, response):
         if re.match(".*play\.google\.com.*",response.url):
-            for pagenum in range(0,48,24):
+            for pagenum in range(0,2000,24):
                 url = re.sub(r"start=\d+","start="+str(pagenum),response.url)
                 yield Request(url, callback=self.parse_url)
 
@@ -32,40 +32,42 @@ class GoogleplaySpider(Spider):
 
 
     def parse_app(self, response):
-	print "------------------------------------------------------"
-	print response.url
-	items = []
-	sel = Selector(response)
-	i = TraversalPlayItem()
-	i['url'] = response.url
-	i['md5'] = md5(response.url).hexdigest()
-	i['name'] = sel.xpath('//*[@class="document-title"]/div/text()').extract()
-	i['devid'] = sel.xpath('//*[@itemprop="author"]/meta/@content').extract()
-	i['icon'] = sel.xpath('//*[@class="cover-container"]/img/@src').extract()
-	i['images'] = sel.xpath('//*[@class="screenshot"]/@src').extract()
-	i['category'] = category1
-	i['whatsnew'] = sel.xpath('//*[@class="recent-change"]/text()').extract()
-	i['datepublished'] = sel.xpath('//*[@itemprop="datePublished"]/text()').extract()
-	i['filesize'] = sel.xpath('//*[@itemprop="fileSize"]/text()').extract()
-	i['numdownload'] = sel.xpath('//*[@itemprop="numDownloads"]/text()').extract()
-	i['version'] = sel.xpath('//*[@itemprop="softwareVersion"]/text()').extract()
-	i['requirementsos'] =  sel.xpath('//*[@itemprop="operatingSystems"]/text()').extract()
-	i['content_rating'] = sel.xpath('//*[@itemprop="contentRating"]/text()').extract()
-	i['devweb'] = sel.xpath('//*[@class="dev-link"]/@href').extract()[0]
-	i['devemail'] = sel.xpath('//*[@class="dev-link"]/@href').extract()[1]
-	i['score'] = sel.xpath('//*[@class="score"]/text()').extract()
-	i['reviewsnum'] = sel.xpath('//*[@class="reviews-num"]/text()').extract()
-	i['star5num'] =  sel.xpath('//*[@class="rating-bar-container five"]/span[3]/text()').extract()
-	i['star4num'] =  sel.xpath('//*[@class="rating-bar-container four"]/span[3]/text()').extract()
-	i['star3num'] =  sel.xpath('//*[@class="rating-bar-container three"]/span[3]/text()').extract()
-	i['star2num'] =  sel.xpath('//*[@class="rating-bar-container two"]/span[3]/text()').extract()
-	i['star1num'] =  sel.xpath('//*[@class="rating-bar-container one"]/span[3]/text()').extract()
+        print "------------------------------------------------------"
+        print response.url
 
-	items.append(i)
+        items = []
+        sel = Selector(response)
+    	i = TraversalPlayItem()
+    	i['url'] = response.url
+        i['md5'] = md5(response.url).hexdigest()
+    	i['name'] = sel.xpath('//*[@class="document-title"]/div/text()').extract()
+    	i['devid'] = sel.xpath('//*[@itemprop="author"]/meta/@content').extract()
+    	i['icon'] = sel.xpath('//*[@class="cover-container"]/img/@src').extract()
+    	i['images'] = sel.xpath('//*[@class="screenshot"]/@src').extract()
+    	i['category'] = category1
+    	i['whatsnew'] = sel.xpath('//*[@class="recent-change"]/text()').extract()
+    	i['datepublished'] = sel.xpath('//*[@itemprop="datePublished"]/text()').extract()
+    	i['filesize'] = sel.xpath('//*[@itemprop="fileSize"]/text()').extract()
+    	i['numdownload'] = sel.xpath('//*[@itemprop="numDownloads"]/text()').extract()
+    	i['version'] = sel.xpath('//*[@itemprop="softwareVersion"]/text()').extract()
+    	i['requirementsos'] =  sel.xpath('//*[@itemprop="operatingSystems"]/text()').extract()
+    	i['content_rating'] = sel.xpath('//*[@itemprop="contentRating"]/text()').extract()
+    	i['devweb'] = sel.xpath('//*[@class="dev-link"]/@href').extract()[0]
+        try:
+            i['devemail'] = sel.xpath('//*[@class="dev-link"]/@href').extract()[1]
+        except IndexError,abc:
+            i['devemail'] = "null"
+    	i['score'] = sel.xpath('//*[@class="score"]/text()').extract()
+    	i['reviewsnum'] = sel.xpath('//*[@class="reviews-num"]/text()').extract()
+        i['star5num'] =  sel.xpath('//*[@class="rating-bar-container five"]/span[3]/text()').extract()
+    	i['star4num'] =  sel.xpath('//*[@class="rating-bar-container four"]/span[3]/text()').extract()
+        i['star3num'] =  sel.xpath('//*[@class="rating-bar-container three"]/span[3]/text()').extract()
+    	i['star2num'] =  sel.xpath('//*[@class="rating-bar-container two"]/span[3]/text()').extract()
+        i['star1num'] =  sel.xpath('//*[@class="rating-bar-container one"]/span[3]/text()').extract()
 
+        items.append(i)
 
-	for a in items:
-     	    print a	
+        for a in items:
+            print a
 
-	return items
- 
+        return items
